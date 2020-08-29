edac293e605d073beacae80f7140865ab57582aa
samaranand <samarss2015@gmail.com>
2020-08-29 23:13:53 +0530

    day2-try1

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {myFunction()};
+
+
+var header = document.getElementById("headid");
+
+
+var sticky = header.offsetTop;
+
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
